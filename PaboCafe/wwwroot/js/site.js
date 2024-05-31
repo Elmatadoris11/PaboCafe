b325b09a084fa4015e18c39c65433caa7ae60bfc
Elmatadoris11 <muqry2005@gmail.com>
2024-05-31 15:14:12 +0800

    Updated

--- PaboCafe/wwwroot/js/site.js
+++ PaboCafe/wwwroot/js/site.js
@@ -20,3 +20,32 @@ function prevImage() {
 function nextImage() {
     showImage(currentImageIndex + 1);
 }
+
+const stats = document.querySelector('.stats');
+const counters = document.querySelectorAll(".counter");
+let bol = false;
+
+const sectionOffset = stats.offsetTop + stats.offsetHeight;
+
+window.addEventListener("scroll", () => {
+    const pageOffset = window.innerHeight + pageYOffset;
+    if (pageOffset > sectionOffset && bol === false) {
+        counters.forEach((counter) => {
+            function updateCount() {
+                const target = +counter.getAttribute('data-target');
+                const speed = +counter.getAttribute('data-speed');
+                let count = +counter.innerText;
+
+                if (count < target) {
+                    counter.innerText = count + 1;
+                    setTimeout(updateCount, speed);
+                    count++;
+                } else {
+                    counter.innerText = target;
+                }
+            }
+            updateCount();
+            bol = true;
+        });
+    }
+});
